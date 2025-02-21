c2b46888a2dcc0c79a1620f4b3d6da778b7d43ad
Sergio <pekozamora34@gmail.com>
2025-02-20 19:43:52 -0700

    Se realizo el usse effect

---
+++ conta_react/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count > 10) {
+      alert("El contador no puede superar 10");
+      setCount(10);
+    }
+  }, [count]); 
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={() => setCount(0)}>Resetear</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App;
