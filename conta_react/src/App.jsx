d88f14a3991c2129056282ed277b970367f840f0
Sergio <pekozamora34@gmail.com>
2025-02-25 18:33:17 -0700

    Se agrego una tabla en react

--- conta_react/src/App.jsx
+++ conta_react/src/App.jsx
@@ -1,7 +1,27 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(nombre, apellido, edad) {
+  return { nombre, apellido, edad};
+}
+
+const rows = [
+  createData("Sergio", "Lopez", 19),
+  createData("Paulina", "Zamora", 22),
+  createData("Gabriel", "Alarcon", 26),
+  createData("Jose", "Rodriguez", 20),
+  createData("Maria", "Ayala", 19),
+];
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,7 +31,8 @@ function App() {
       alert("El contador no puede superar 10");
       setCount(10);
     }
-  }, [count]); 
+  }, [count]);
+
   return (
     <>
       <div>
@@ -24,19 +45,40 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <button onClick={() => setCount(0)}>Resetear</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nombre</TableCell>
+              <TableCell align="right">Appellido</TableCell>
+              <TableCell align="right">Edad</TableCell>
+              
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.nombre} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                <TableCell component="th" scope="row">
+                  {row.nombre}
+                </TableCell>
+                <TableCell align="right">{row.apellido}</TableCell>
+                <TableCell align="right">{row.edad}</TableCell>
+                
+                
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
 export default App;
